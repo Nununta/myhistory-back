a291e0300faf3955c472ddeff8dbbbb0afece570
Nununta <yutayuki09040827@gmail.com>
2021-12-23 18:41:03 +0900

    環境構築第一段階

--- MyHistory/routes/api.php
+++ MyHistory/routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//apiTest
+Route::get('/hello', function() {
+    return response()->json(['hello' => 'helloworld'],201);
+});
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
